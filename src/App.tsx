385e6d8afec48408d506bf177c17d97645c946f4
quochuy2405 <huy01682312920@gmail.com>
2022-07-14 09:19:29 +0700

    update router can be redirect

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layouts />}>
           <Route index element={<Home />} />
-          <Route path="/a" element={<Register />} />
+          <Route path="/test" element={<Register />} />
         </Route>
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Page404 />} />
